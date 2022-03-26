0b62f06d8e35ad8d978ce7c0618c904b166e195f
KirdesGrvl <cedgourville@gmail.com>
2022-03-26 19:04:12 +0100

    first commit

---
+++ app/root.tsx
@@ -0,0 +1,40 @@
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from 'remix';
+import type { MetaFunction, LinksFunction } from 'remix';
+
+import resetStyles from './styles/reset.css';
+import unocssStyles from './styles/unocss.css';
+
+export const links: LinksFunction = () => [
+  { rel: 'stylesheet', href: resetStyles },
+  { rel: 'stylesheet', href: unocssStyles },
+];
+
+export const meta: MetaFunction = () => ({
+  charset: 'utf-8',
+  title: 'New Remix App',
+  viewport: 'width=device-width,initial-scale=1',
+});
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
---
+++ app/routes/index.tsx
@@ -0,0 +1,7 @@
+export default function Index() {
+  return (
+    <div className="grid gap-10">
+      <h1>Welcome to Vercel Ced</h1>
+    </div>
+  );
+}
