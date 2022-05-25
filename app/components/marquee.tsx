c2e9e63bb1e7947a1f91844a9b34d3cbe8aa804b
KirdesGrvl <cedgourville@gmail.com>
2022-05-25 06:12:33 +0200

    wip: marquee and animation

--- app/components/marquee.tsx
+++ app/components/marquee.tsx
@@ -1,9 +1,9 @@
 export function Marquee(props: { content: string }) {
   return (
-    <div className="fixed w-full top-0 bg-[var(--black)] border-b-1 border-white">
-      <div className="overflow-x-hidden h-[5rem] whitespace-nowrap flex items-center justify-center gap-x-10">
+    <div className="marquee">
+      <div className="marquee__inner h-[2.5rem]">
         {Array.from({ length: 5 }, (_, idx) => (
-          <p key={idx}>{props.content}</p>
+          <span key={idx}>{props.content}</span>
         ))}
       </div>
     </div>
