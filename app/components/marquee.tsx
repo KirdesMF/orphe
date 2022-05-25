7c8b1ba9cf9ad378703b8ac190cc422e60572694
KirdesGrvl <cedgourville@gmail.com>
2022-05-25 03:14:49 +0200

    improve design

--- app/components/marquee.tsx
+++ app/components/marquee.tsx
@@ -1,6 +1,6 @@
 export function Marquee(props: { content: string }) {
   return (
-    <div className="fixed w-full top-0 bg-[var(--black)]">
+    <div className="sticky w-full top-0 bg-[var(--black)]">
       <div className="overflow-x-hidden h-[5rem] whitespace-nowrap flex items-center gap-x-10">
         {Array.from({ length: 5 }, (_, idx) => (
           <p key={idx}>{props.content}</p>
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,13 +1,19 @@
 import { getSongs, supabase } from '~/utils/supabase.server';
 import { commitDataSession, getDataSession } from '~/utils/cookie.server';
 import { AudioPlayer } from '~/components/audio-player';
-import { CCVSVG, OBProdSVG, OrpheGreekSVG } from '~/components/custom-svg';
+import {
+  CCVSVG,
+  OBProdSVG,
+  OrpheGreekSVG,
+  OrpheLyreSVG,
+} from '~/components/custom-svg';
 import { json } from '@remix-run/node';
 import { useLoaderData } from '@remix-run/react';
 
 import type { Song } from '~/models/song';
 import type { LoaderFunction, ActionFunction } from '@remix-run/node';
 import { InstagramSVG, SnapChatSVG, TikTokSVG } from '~/components/icons';
+import { Marquee } from '~/components/marquee';
 
 type Loader = {
   songs: Array<Song>;
@@ -74,24 +80,32 @@ export default function Index() {
   const { songs, user_likes } = useLoaderData<Loader>();
 
   return (
-    <main className="px-4xl">
+    <main className="px-4xl relative">
+      <span
+        aria-hidden
+        className="absolute w-50 h-50 text-white -top-10 -left-10 -rotate-15"
+      >
+        <OrpheLyreSVG />
+      </span>
+
       <section className="min-h-[100vh] flex flex-col items-center justify-center">
-        <div className="grid place-items-center gap-y-5xl">
+        <article className="grid place-items-center gap-y-6xl">
           <a
             href="https://www.instagram.com/obprod/"
-            className="text-white grid place-items-center"
+            className=" grid place-items-center"
+            aria-label="Instagram ObProd"
           >
-            <span className="h-15 w-15">
+            <span className="h-15 w-15 text-white hover:text-[var(--red)] ease-in duration-200">
               <OBProdSVG />
             </span>
           </a>
 
-          <h1 className="grid">
+          <h1 className="grid place-items-center">
             <span className="sr-only">Orphe CCV NetTape</span>
-            <span className="overflow-hidden w-[20vmax]">
+            <span aria-hidden className="overflow-hidden w-[15vmax]">
               <OrpheGreekSVG />
             </span>
-            <span className="overflow-hidden w-[20vmax]">
+            <span aria-hidden className="overflow-hidden w-[18vmax]">
               <CCVSVG />
             </span>
           </h1>
@@ -102,28 +116,44 @@ export default function Index() {
               href="https://www.instagram.com/orphe350/"
               target="_blank"
               rel="noopener noreferrer"
+              aria-label="Instagram Orphe"
             >
               <InstagramSVG />
             </a>
+
             <a
               className="text-white h-8 w-8"
               href="https://www.snapchat.com/add/badou-orlov"
               target="_blank"
               rel="noopener noreferrer"
+              aria-label="Snapchat Orphe"
             >
               <SnapChatSVG />
             </a>
+
             <a
               className="text-white h-8 w-8"
               href="https://www.tiktok.com/@orphe350"
               target="_blank"
               rel="noopener noreferrer"
+              aria-label="TikTok Orphe"
             >
               <TikTokSVG />
             </a>
           </nav>
-        </div>
+        </article>
       </section>
+
+      <article className="grid gap-y-5 justify-center text-2xl">
+        <p className="max-w-[35ch] text-center">
+          En attendant son premier projet, découvrez l'univers d'
+          <span className="text-[var(--red)]"> Orphe</span> à travers sa NetTape
+          <span className="text-[var(--red)]"> CCV</span>. Les musiques sont
+          disponibles en streaming ou en téléchargement
+          <span className="text-[var(--red)]"> 100% gratuit</span>.
+        </p>
+      </article>
+
       <section className="min-h-[100vh] flex flex-col justify-center items-center">
         <AudioPlayer songs={songs} user_likes={user_likes} />
       </section>
